7bf9f127c7573d3b5570f1a492b070b39e58d3e8
tinatuazon <tuazoncristina31@gmail.com>
2025-11-04 15:12:52 +0800

    Add edit button.

--- app/components/user-card.tsx
+++ app/components/user-card.tsx
@@ -5,7 +5,7 @@ import { Badge } from '@/components/ui/badge'
 import { Phone, Mail } from 'lucide-react'
 import { User } from '@/app/actions/schemas'
 import DeleteButton from './delete-button'
-// import { UserEditDialog } from './user-edit-dialog'
+import { UserEditDialog } from './user-edit-dialog'
 
 interface UserCardProps {
   user: User
@@ -44,7 +44,7 @@ export default function UserCard({ user }: UserCardProps) {
       </CardContent>
       <CardFooter className="flex justify-between items-center">
         <DeleteButton userId={user.id} />
-        {/* <UserEditDialog user={user} /> */}
+        <UserEditDialog user={user} />
       </CardFooter>
     </Card>
   );
